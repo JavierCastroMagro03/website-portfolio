b6fedc1082cce34c7c887e9ce388d443b1ad8d05
JavierCastroMagro03 <146874471+JavierCastroMagro03@users.noreply.github.com>
2025-10-02 13:53:52 +0200

    Actu II
    Pruebo a cambiar la ruta que creo que la escribí mal

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ export const metadata: Metadata = {
   description: "Estudiante de Diseño y Desarrollo de Videojuegos. En este portfolio presento mis habilidades, herramientas y formación, así como proyectos y cursos que respaldan mi desarrollo. Madrid-España",
   icons: {
     icon: [
-      { url: "/Foto_Perfil_32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/Foto_Perfil_64x64.png", sizes: "64x64", type: "image/png" },
+      { url: "/public/img/Foto_Perfil_32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/public/img/Foto_Perfil_64x64.png", sizes: "64x64", type: "image/png" },
     ],
-    apple: "/Foto_Perfil_64x64.png", // icono en iOS
+    apple: "/public/img/Foto_Perfil_64x64.png", // icono en iOS
   },
 };
 
